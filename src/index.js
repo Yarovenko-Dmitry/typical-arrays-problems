f7c204a81fd3e2aa8460c368fa44b36170fe3aa0
prostiak2019 <yarovenkodmv@gmail.com>
2020-03-20 19:35:24 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    if(!array || array.length == 0) return 0;
+    array.sort(function(a, b) {
+      return a - b ;
+    }); 
+    return  array[0];
+  }
+  
+  exports.max = function max (array) {
+    if(!array || array.length == 0) return 0;
+    array.sort(function(a, b) {
+      return a - b ;
+    }); 
+    return array[array.length - 1];
+  }
+  
+  exports.avg = function avg (array) {
+    if(!array || array.length == 0) return 0;
+    let result = 0;
+    for (let i = 0; i <= (array.length - 1); i++) {
+      result = result + array[i];      
+    }
+    result = result / array.length; 
+    return result;
+  }
